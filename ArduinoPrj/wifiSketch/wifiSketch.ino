ecb43a329d970e4d53d6a281ef6456ddf2b31be5
brejcmic <brejcmic@fel.cvut.cz>
2016-03-27 17:53:19 +0200

    Funkcni servisni log a predelana koncepce hledani retezcu. Vystup obou seriovych linek se zapisuje do FIFO, ve kterem se nasledne hleda retezec.

--- ArduinoPrj/wifiSketch/wifiSketch.ino
+++ ArduinoPrj/wifiSketch/wifiSketch.ino
@@ -8,20 +8,28 @@ SoftwareSerial softSerial(10, 11); // RX, TX
 #define ESP8266CHARCT 20// pocet najednou odesilanych znaku
 #define ESP8266TXPER  10// perioda vysilani v ms
 #define COM_WATCHDOG_TIME   3000// cas v ms, po kterem se resetuje stav RX
-#define COM_URL_LENGTH 80 //maximalni delka retezce URL
+#define COM_RX_LEN    32 //delka fifa pro prijem z esp8266
+#define COM_SR_LEN    8 //delka fifa pro prijem ze servisni linky
+#define COM_MSG_LEN   64 //delka kruhoveho bufferu pro ulozeni servisni zpravy, mocnina 2
+#define COM_MSG_MSK   COM_MSG_LEN-1 //maska pro pretekani indexu bufferu servisni zpravy
 
-#define DEBUG       1
+#define DEBUG       0
 #define debugSer    Serial
 
 #if DEBUG == 1
 #define PRTDBG(x)   debugSer.println(F(x)) //jen pro hlaseni
 #define WRTDBG(x)   debugSer.write(x) //bez konverze
 #define WRCDBG(x)   debugSer.print(x) //s konverzi
+#define wrLog(x, b) debugSer.write(x) //trvaly vypis logu
+#define wrMsg(x)    debugSer.println(F(x)) //vypis zpravy
 #else
 #define PRTDBG(x)
 #define WRTDBG(x)
 #define WRCDBG(x)
+#define wrLog(x, b) if(b) debugSer.write(x) //podmineny vypis logu
+#define wrMsg(x)    debugSer.println(F(x)) //vypis zpravy
 #endif
+
 
 const char headhttp[] = {"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nConnection: close\r\nContent-Length: "};//nechat v ramce
 const char mainpage[] PROGMEM = {"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"><html><head><title>Aquaduino</title><link href=\"data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII=\" rel=\"icon\" type=\"image/x-icon\" /></head><body>Ahoj<form action=\"/wtf\"><input type=\"submit\" name=\"do\" value=\"zhasni\"><input type=\"submit\" name=\"do\" value=\"rozsvit\"></form> </body></html>"};
@@ -31,8 +39,12 @@ typedef enum{
 }txStates_t;
 
 typedef enum{
-  WAITRX, CLIENTRX, COMMARX, BYTECNTRX, REQRX, URLRX, FLUSHRX
+  WAITRX, CLIENTRX, COMMARX, BYTECNTRX, REQRX, URLRX, FLUSHRX, SERVISMODERX
 }rxStates_t;
+
+typedef enum{
+  LOGSR, WAITSR, ATCOMMSR, ATSENDSR
+}srStates_t;
 
 void waitFor(const char* string)
 {
@@ -90,36 +102,43 @@ void loop() {
   com_monitor();
 }
 
-//Funkce hleda retezec v poslednich 8 prijatych znacich
-//Pokud nema byt pridan znak do fifa, je treba predat znak "\0"
-int com_findString(const char* cArr, char recChar)
+//Funkce hleda retezec v poslednich flen prijatych znacich.
+unsigned int com_findInFifo(const char* cArr, const char *fifo, unsigned int flen)
 {
-  static char fifo[8]; //fifo poslednich 8 znaku
-  static unsigned int idxf = 0; //index fifa
+  unsigned int ret; //navratova hodnota
   unsigned int j; //index znaku v retezci
-  unsigned int i; //index index znaku ve fifo
 
-  //pridani znaku do fifa
-  if(recChar != '\0')
-  {
-    fifo[idxf] = recChar;
-    idxf++;
-    idxf &= 0x07;
-  }
   //zjisteni delky retezce
   j = 0;
-  while(cArr[j] != '\0') j++;
-  if(j > 8 || j == 0) return 0;
-
-  //hledani retezce ve fifo
-  i = idxf;
-  while(j > 0 && (fifo[i] == cArr[--j]))
+  while(cArr[j] != '\0')
   {
-    i--;
-    i &= 0x07;
+    j++;
   }
-  if(j == 0) return 1;
-  else return 0;
+  
+  ret = (j != 0 && j <= flen);
+  flen = 0;
+  //hledani retezce ve fifo
+  while(j > 0 && ret)
+  {
+    j--;
+    ret = (fifo[flen] == cArr[j]);
+    flen++;
+  }
+  return ret;
+}
+//Funkce nahraje novy znak do fifo, novy znak bude mit index 0.
+void com_putCharInFifo(char nChar, char *fifo, unsigned int flen)
+{
+  unsigned int b;
+
+  b = (flen > 1);
+  while(b)
+  {
+    flen--;
+    fifo[flen] = fifo[flen-1];
+    b = (flen > 0);
+  }
+  fifo[0] = nChar;
 }
 
 unsigned int com_delay(unsigned long timeDelay)
@@ -137,23 +156,35 @@ unsigned int com_delay(unsigned long timeDelay)
   return ret;
 }
 
+unsigned int com_putCharInCyrcBuff(char nChar, char *cArr, unsigned int msk)
+{
+  static unsigned int idx = 0;
+  idx++;
+  idx &= msk;
+  cArr[idx] = nChar;
+}
+
 void com_monitor(void)
 {
   //rx-----------------------------------------------
   static rxStates_t rxState = WAITRX;//stav prijmu
   static int rxLength; //pocet prijatych byte (tachometr)
   static int rxch = 0;//index nacteneho znaku
-  static char pageUrl[COM_URL_LENGTH];//jmeno pozadovane stranky v GET
-  static int urlIdx = 0;//index zapisu do pole
+  static char rxFifo[COM_RX_LEN];//Fifo pro prijem
   //tx-----------------------------------------------
   static txStates_t txState = WAITTX;//stav vysilani
   static txStates_t reqState = WAITTX;//pozadovany stav vysilani dle prijmu
   static txStates_t ackState = WAITTX;//stav pro vysilani po obdrzeni znaku ">"
   static int client; //klient
   static int txch = 0; //index aktualne vysilaneho znaku
+  //servis-------------------------------------------
+  static srStates_t srState = WAITSR;//stav servisni linky
+  static char srFifo[COM_SR_LEN];//servisni fifo prijmu
+  static char srMsg[COM_MSG_LEN];//buffer pro servisni zpravu
+  static char srIdxMsg; //index bufferu pro servisni zpravu
   //obecne-------------------------------------------
   char dataByte; //nacteny nebo vysilany znak
-  String page; //prijaty retezec retezec pro vysilani
+  String page; //retezec pro vysilani
   int idx; //index pro cyklus for
 
   //RX------------------------------------------------------
@@ -161,13 +192,15 @@ void com_monitor(void)
   while(esp8266Ser.available())
   {
     dataByte = esp8266Ser.read();
+    com_putCharInFifo(dataByte, rxFifo, COM_RX_LEN);
     rxch++; //pocet prectenych znaku zvysit o 1
-    WRTDBG(dataByte);
+    wrLog(dataByte, (srState == LOGSR));
     switch(rxState)
     {
       case WAITRX:
-        if(com_findString("+IPD,", dataByte))
+        if(com_findInFifo("+IPD,", rxFifo, COM_RX_LEN))
         {
+          PRTDBG("\nPrijem");
           rxState = CLIENTRX;//zaznamenan prijem
         }
         if(dataByte == '>' && txState == ACKPGTX)
@@ -197,6 +230,7 @@ void com_monitor(void)
           else //kdyz je volno ke zpracovani
           {
             rxState = REQRX;
+            PRTDBG("\ncekam na GET");
           }
         }
         else
@@ -207,9 +241,8 @@ void com_monitor(void)
         break;
         
       case REQRX://cekani na GET
-        if(com_findString("GET ", dataByte))
+        if(com_findInFifo("GET ", rxFifo, COM_RX_LEN))
         {
-          urlIdx = 0;
           rxState = URLRX;
         }
         else if(rxch > 5) //nenalezeno GET
@@ -221,42 +254,38 @@ void com_monitor(void)
       case URLRX://cteni jmena stranky
         if(dataByte == ' ')
         {
-          pageUrl[urlIdx] = '\0';
-          page = String(pageUrl);
           //identifikace stranky
-          if(page == String("/favicon.ico")) 
+          if(com_findInFifo("/favicon.ico ", rxFifo, COM_RX_LEN))
           {
+            PRTDBG("\nFavicon");
             reqState = REQSNDPGTX;
           }
-          else if(page == String("/"))
+          else if(com_findInFifo("/ ", rxFifo, COM_RX_LEN))
           {
+            PRTDBG("\nMainpage");
             reqState = REQSNDPGTX;
           }
-          else if(page == String("/wtf?do=rozsvit"))
+          else if(com_findInFifo("/wtf?do=rozsvit ", rxFifo, COM_RX_LEN))
           {
+            PRTDBG("\nTlacitko zapni");
             reqState = ONBUTTX;
           }
-          else if(page == String("/wtf?do=zhasni")) 
+          else if(com_findInFifo("/wtf?do=zhasni ", rxFifo, COM_RX_LEN))
           {
+            PRTDBG("\nTlacitko vypni");
             reqState = OFFBUTTX;
           }
           else 
           {
+            PRTDBG("\nStranka neidentifikovana");
             reqState = WAITTX;
           }
-          
           rxState = FLUSHRX;
-        }
-        else
-        {
-          pageUrl[urlIdx] = dataByte;
-          if(urlIdx < (COM_URL_LENGTH-1)) urlIdx++;
-          else rxState = FLUSHRX; //stranku nelze identifikovat
         }
         break;
         
       case FLUSHRX://vycteni zbytku bufferu
-        if(rxch >= (rxLength))
+        if(rxch >= rxLength)
         {
           rxState = WAITRX;
           PRTDBG("\nBuffer uvolnen");
@@ -277,6 +306,11 @@ void com_monitor(void)
       {
         com_delay(0);//znovunastaveni casu
         txState = reqState;
+        if(srState == ATSENDSR) //odeslani servisni zpravy
+        {
+          esp8266Ser.println(srMsg);
+          srState = LOGSR;
+        }
       }
       else if(com_delay(COM_WATCHDOG_TIME))//WATCHDOG FOR RX
       {
@@ -351,6 +385,53 @@ void com_monitor(void)
       txState = WAITTX;
       ackState = WAITTX;
       break;
+  }
+  //SERVIS------------------------------------------------------
+  while(debugSer.available())
+  {
+    dataByte = debugSer.read();
+    com_putCharInFifo(dataByte, srFifo, COM_SR_LEN);
+
+    switch(srState)
+    {
+      case LOGSR: //vypisuje log az do prijeti SR+END
+        if(com_findInFifo("SR+END", srFifo, COM_SR_LEN))
+        {
+          wrMsg("\nSR+END\n\nOK");
+          srState = WAITSR;
+          break;
+        }
+      case WAITSR:
+        if(com_findInFifo("AT", srFifo, COM_SR_LEN))
+        {
+          srMsg[0] = 'A';
+          srMsg[1] = 'T';
+          srIdxMsg = 1;
+          srState = ATCOMMSR;
+        }
+        break;
+
+      case ATCOMMSR:
+        srIdxMsg++;
+        srIdxMsg &= COM_MSG_MSK;
+        if(dataByte == '\n')
+        {
+          srMsg[srIdxMsg] = '\0';
+          srState = ATSENDSR;
+        }
+        else
+        {
+          srMsg[srIdxMsg] = dataByte;
+        }
+        break;
+
+      case ATSENDSR: //ceka na odeslani servisni zpravy
+        break;
+        
+      default:
+        srState = WAITSR;
+        break;
+    }
   }
 }
 
